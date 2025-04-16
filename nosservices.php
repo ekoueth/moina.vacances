e0b039569e0495e84e225fefac6fe2ee997400ac
REINE EKOUETH <reineekoueth@gmail.com>
2025-04-16 14:25:21 +0200

    Add files via upload

---
+++ nosservices.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nos Services - Moina Vacances</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <header>
+        <div class="top-page">
+        <a href="index.html"><img src="images/logo.jpg" alt="Logo Moina Vacances"></a>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.html">Accueil</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section class="service">
+        <h1>Nos Services</h1>
+        <div class="liens">
+            <a href="inscription.php" class="clique">Inscrivez-vous</a>
+        </div>
+        <div class="liens">
+            <a href="destination.php" class="clique">Nos Destinations</a>
+        </div>
+    </section>
+</body>
+</html>
